221f239f98eab74121c8b61700c78c2012f1fee9
ねむ <myproxy0108@gmail.com>
2026-07-09 10:01:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -918,7 +918,7 @@ app.post('/webhook', async (req, res) => {
     })();
 });
 
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(process.env.PORT || 3000, () => console.log(`Live V41 PERFECT FINAL`));
-}
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Run ${PORT}`));
+
 module.exports = app;
